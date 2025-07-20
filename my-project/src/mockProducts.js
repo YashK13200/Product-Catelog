d68eaab77db38c1523b7ad22e5c2a2b0c6a4e030
Yash Kashyap <yashkashyap131@gmail.com>
2025-07-20 17:55:08 +0530

    first commit

---
+++ my-project/src/mockProducts.js
@@ -0,0 +1,31 @@
+export const mockProducts = [
+    {
+        id: 1,
+        name: "Apple iPhone 15",
+        category: "SmartPhones",
+        barnd: "Apple",
+        price: 1200,
+    },
+    {
+        id: 2,
+        name: "Samsung Galaxy S24",
+        category: "SmartPhones",
+        barnd: "Samsung",
+        price: 900,
+    },
+    {
+        id: 3,
+        name: "Sony WH-12",
+        category: "Headphones",
+        barnd: "Sony",
+        price: 380,
+    },
+    {
+        id: 4,
+        name: "Dell XPS 13",
+        category: "Laptops",
+        barnd: "Dell",
+        price: 1200,
+    },
+
+]
